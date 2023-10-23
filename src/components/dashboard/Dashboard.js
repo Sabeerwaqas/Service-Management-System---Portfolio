f00597a988fd5eff5a3683fd1a16faf95aab9f80
Sabeerwaqas <sabeerwaqas050@gmail.com>
2023-10-23 13:41:23 +0500

    [Working on dashboard]

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./dashboard.css";
 import { Grid } from "@mui/material";
-import profileCardImage from "../images/profileCard.gif"
+import profileCardImage from "../images/profileImg.jpeg";
+// import profileImage from "../images/profileImg.jpeg";
 
 const Dashboard = () => {
   return (
@@ -12,12 +13,23 @@ const Dashboard = () => {
               <div className=" child-one">
               <img src={profileCardImage} className="profile-card-img" alt="service management system gif" />
               </div>
-              <div className="child-three">2</div>
-              <div className="child-two" >3</div>
-              <div className="child-four">4</div>
+              <div className="child-two" >
+              <h3>Service Managing Tool</h3>
+              </div>
+              <div className="child-four">
+                <ul>
+                  <li className="list-child">Digital marketing services</li>
+                </ul>
+              </div>
           </Grid>
-          <Grid item md={6} className="same-classes services-cards">w</Grid>
-          <Grid item md={2} className="same-classes ">e</Grid>
+          <Grid item md={6} className="same-classes services-cards">
+            <div>
+              <h2 className="digital-marketing-clients">
+                Digital Marketing Clients
+              </h2>
+            </div>
+          </Grid>
+          <Grid item md={2} className="same-classes">e</Grid>
         </Grid>
       </div>
     </>
