d9250f98e72eeec48f196ffde9e9abe7a6ede2c0
Sabeerwaqas <sabeerwaqas050@gmail.com>
2023-10-23 12:17:01 +0500

    [Built: Start working on making company profile card]

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,15 +1,23 @@
 import React from "react";
 import "./dashboard.css";
 import { Grid } from "@mui/material";
+import profileCardImage from "../images/profileCard.gif"
 
 const Dashboard = () => {
   return (
     <>
       <div>
-        <Grid container spacing={2}>
-          <Grid item md={3} className="same-classes company profile card"></Grid>
-          <Grid item md={6} className="same-classes services-cards"></Grid>
-          <Grid item md={3} className="same-classes "></Grid>
+        <Grid container rowGap={2} columnGap={3}>
+          <Grid item md={2}  className="same-classes company-profile-card">
+              <div className=" child-one">
+              <img src={profileCardImage} className="profile-card-img" alt="service management system gif" />
+              </div>
+              <div className="child-three">2</div>
+              <div className="child-two" >3</div>
+              <div className="child-four">4</div>
+          </Grid>
+          <Grid item md={6} className="same-classes services-cards">w</Grid>
+          <Grid item md={2} className="same-classes ">e</Grid>
         </Grid>
       </div>
     </>
