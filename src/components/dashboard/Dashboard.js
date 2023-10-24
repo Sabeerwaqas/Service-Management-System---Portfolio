f1c5b14458286cc653c8b8b4a67aac7c70990269
Sabeerwaqas <sabeerwaqas050@gmail.com>
2023-10-24 14:03:57 +0500

    [Redux-Toolkit setup]

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -9,27 +9,46 @@ const Dashboard = () => {
     <>
       <div>
         <Grid container rowGap={2} columnGap={3}>
-          <Grid item md={2}  className="same-classes company-profile-card">
-              <div className=" child-one">
-              <img src={profileCardImage} className="profile-card-img" alt="service management system gif" />
-              </div>
-              <div className="child-two" >
+          <Grid item md={3} className="same-classes company-profile-card">
+            <div className=" child-one"></div>
+            <div className="child-two">
               <h3>Service Managing Tool</h3>
-              </div>
-              <div className="child-four">
-                <ul>
-                  <li className="list-child">Digital marketing services</li>
-                </ul>
-              </div>
+            </div>
+            <div className="child-four">
+              <ul>
+                <li className="list-child">Digital marketing services</li>
+              </ul>
+            </div>
           </Grid>
-          <Grid item md={6} className="same-classes services-cards">
+          <Grid item md={7} className="same-classes services-cards">
             <div>
               <h2 className="digital-marketing-clients">
                 Digital Marketing Clients
               </h2>
+
+              <Grid container>
+                <Grid item md={2}>
+                  <div className="client-data-card">
+                    <div>
+                      <h4 className="client-data">Client Name:</h4>
+                    </div>
+                    <div>
+                      <h4 className="client-data">Client Phone:</h4>
+                    </div>
+                    <div>
+                      <h4 className="client-data">Onboarding Date:</h4>
+                    </div>
+                    <div>
+                      <h4 className="client-data"> Service Status:</h4>
+                    </div>
+                    <div>
+                      <h4 className="client-data"> Service Fee:</h4>
+                    </div>
+                  </div>
+                </Grid>
+              </Grid>
             </div>
           </Grid>
-          <Grid item md={2} className="same-classes">e</Grid>
         </Grid>
       </div>
     </>
